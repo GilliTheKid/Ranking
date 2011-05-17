66d2c5ab972669c61ed8be328d5f0221ad17d28f
Andrew Gillibrand (ADRGLT) <andrew@upstream.info>
2011-05-17 17:51:50 +0200

    Added /monkey

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Ranking::Application.routes.draw do
   # Sample resource route (maps HTTP verbs to controller actions automatically):
     resources :home
     resources :player
+    resources :monkey
 
   # Sample resource route with options:
   #   resources :products do
