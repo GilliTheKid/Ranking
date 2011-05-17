2ea4953c03ebd992ef316dd6db1ce269069e8b02
Andrew Gillibrand (ADRGLT) <andrew@upstream.info>
2011-05-17 16:08:09 +0200

    Add Rails App Ranking

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ranking::Application.config.secret_token = 'd7b9f010a7ccae2f73b6b01df5da778822005962757f770d1cad6f49d8da13411ef71c25a1dd15b371e4e29ca880c94c25917e45bfba63677b280458fd7177d0'
