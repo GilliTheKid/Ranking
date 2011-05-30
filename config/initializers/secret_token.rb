7aef8812b85a299cae1222aedc778876972d6e18
GtK <GilliTheKid@GtK-MA.local>
2011-05-30 11:55:30 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ranking::Application.config.secret_token = 'a13a9752b694fe876ea46da79b5c0852ca5d1e21bf01858449ac4bc0d2005f31d82f536f4312670e7cd17bd45a06e377451d6bf89aafa5f7172804bd24d6c4a7'
