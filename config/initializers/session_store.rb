fe726abff56075e0dbca344b18b5e9f2ae9cb431
GtK <GilliTheKid@GtK-MA.local>
2011-05-31 14:09:32 +0200

    Fixed config/initializers/session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,10 +1,9 @@
 # Be sure to restart your server when you modify this file.
 
-<<<<<<< HEAD
 Ranking::Application.config.session_store :cookie_store, :key => '_ranking_session'
-=======
+
 Ranking::Application.config.session_store :cookie_store, :key => '_Ranking_session'
->>>>>>> 967ac9673df2378309e7eb343cdf0c4e6cfb6833
+
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
