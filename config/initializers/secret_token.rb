3e2dbc56ceea9bc716664446dfb147e0747b1eaf
GtK <GilliTheKid@GtK-MA.local>
2011-05-31 14:03:53 +0200

    Fixed broken files

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,8 +4,8 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-<<<<<<< HEAD
+
 Ranking::Application.config.secret_token = 'a13a9752b694fe876ea46da79b5c0852ca5d1e21bf01858449ac4bc0d2005f31d82f536f4312670e7cd17bd45a06e377451d6bf89aafa5f7172804bd24d6c4a7'
-=======
+
 Ranking::Application.config.secret_token = 'd7b9f010a7ccae2f73b6b01df5da778822005962757f770d1cad6f49d8da13411ef71c25a1dd15b371e4e29ca880c94c25917e45bfba63677b280458fd7177d0'
->>>>>>> 967ac9673df2378309e7eb343cdf0c4e6cfb6833
+
