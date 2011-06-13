21925eee71c14bdbf294d361a52b75e7b1d5dded
GtK <GilliTheKid@GtK-MA.local>
2011-06-13 16:43:08 +0200

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Ranking::Application.routes.draw do
   match '/about', :to => "pages#about"
   match '/contact', :to => "pages#conract"
   
+  match '/saturdayschool/home', :to => "pages#home"
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
